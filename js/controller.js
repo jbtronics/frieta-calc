7c6d811f066b00804153023c690809737e548305
Jan Böhmer <mail@jan-boehmer.de>
2024-02-25 18:16:32 +0100

    Added badges to showcase the number of individual selections

--- js/controller.js
+++ js/controller.js
@@ -9,6 +9,8 @@ Stimulus.register("frieta", class extends Controller {
     /** The sum in cents */
     sum = 0;
 
+
+
     timeout = null;
 
     connect() {
@@ -23,11 +25,34 @@ Stimulus.register("frieta", class extends Controller {
     reset() {
         this.sum = 0;
         this.updateResult();
+
+        //Hide all badges and reset their value
+        this.buttonTargets.forEach(btn => {
+            let badge = btn.querySelector(".badge");
+            if (badge) {
+                badge.style.display = "none";
+                badge.textContent = "0";
+            }
+        });
     }
 
     _btnClick(event) {
+        //If no value is set, do nothing
+        if (!event.target.dataset.value) {
+            return;
+        }
+
         this.sum += parseInt(event.target.dataset.value);
         this.updateResult();
+
+        //Find the badge inside the button and update its value
+        let badge = event.target.querySelector(".badge");
+        if (badge) {
+            badge.textContent = parseInt(badge.textContent) + 1;
+
+            //And ensure it is visible
+            badge.style.display = "block";
+        }
 
         //Clear the existing timeout and set a new one
         if (this.timeout) {
