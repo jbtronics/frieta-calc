74004f310a7d39c71ad45251da7643a053fdd144
Jan Böhmer <mail@jan-boehmer.de>
2024-02-25 18:04:25 +0100

    Added timeout after which result clears automatically

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,15 @@
 import { Application, Controller } from "./stimulus.js"
 window.Stimulus = Application.start()
 
+const TIMEOUT = 5000; //10 seconds
+
 Stimulus.register("frieta", class extends Controller {
     static targets = [ "result", "button" ]
 
     /** The sum in cents */
     sum = 0;
+
+    timeout = null;
 
     connect() {
         //Register the btnClick method as an event listener for all buttons
@@ -24,5 +28,14 @@ Stimulus.register("frieta", class extends Controller {
     _btnClick(event) {
         this.sum += parseInt(event.target.dataset.value);
         this.updateResult();
+
+        //Clear the existing timeout and set a new one
+        if (this.timeout) {
+            clearTimeout(this.timeout);
+        }
+        this.timeout = setTimeout(() => {
+            this.reset();
+            this.timeout = null;
+        }, TIMEOUT);
     }
 })
